2dad90ab3b3d55eb26006fc48402c74a8537f8d6
Julieta Wenger <wenerjulieta@gmail.com>
2025-02-04 14:57:16 +0200

    week 1 day 3 third commit

--- Week1/Day3/DailyChallenge.py
+++ Week1/Day3/DailyChallenge.py
@@ -78,3 +78,60 @@ wallet = "$1"
 
 ➞ "Nothing"
 """
+    #Create a program that prints a list of the items you can afford in the store with the money you have in your wallet.
+    #Sort the list in alphabetical order.
+    #Return “Nothing” if you can’t afford anything from the store.
+
+print("\nChallenge 2 \n")
+
+def money_converter(money):
+    return int(money[1:].replace(",", '')) 
+
+def affordable_items(dictionary, wallet):
+    affordable = []
+    wallet = money_converter(wallet)
+    for key, value in dictionary.items():
+        value = money_converter(value)
+        if value < wallet:
+            affordable.append(key)
+    if affordable != []:
+        return sorted(affordable)
+    else:
+        return print("Nothing")
+
+print("\nExamples\n")        
+
+items_purchase = {
+  "Water": "$1",
+  "Bread": "$3",
+  "TV": "$1,000",
+  "Fertilizer": "$20"
+}
+wallet = "$300"
+
+print(affordable_items(items_purchase, wallet))
+
+
+items_purchase = {
+  "Apple": "$4",
+  "Honey": "$3",
+  "Fan": "$14",
+  "Bananas": "$4",
+  "Pan": "$100",
+  "Spoon": "$2"
+}
+
+wallet = "$100" 
+
+print(affordable_items(items_purchase, wallet))
+
+items_purchase = {
+  "Phone": "$999",
+  "Speakers": "$300",
+  "Laptop": "$5,000",
+  "PC": "$1200"
+}
+
+wallet = "$1" 
+
+affordable_items(items_purchase, wallet)
