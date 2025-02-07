f8a8d2cf41cc860aac9472f1284f7ff7a38703e6
Julieta Wenger <wenerjulieta@gmail.com>
2025-02-07 13:51:16 +0200

    week 1 friday commit 1

--- Week1/Day5/DailyChallengeAdvancedAlgorithm.py
+++ Week1/Day5/DailyChallengeAdvancedAlgorithm.py
@@ -16,37 +16,36 @@ For example
 1000 and 2728 sums to the target_number 3728
 1864 and 1864 sums to the target_number 3728
 """
+
 import random
 
+list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 
 target_number   = 3728
 list_of_numbers = [num for num in list_of_numbers if num<=target_number ]
 list_of_numbers_copy = [num for num in list_of_numbers if num<=target_number ]
+print(list_of_numbers)
 
-print(len(list_of_numbers))
-LEN_LIST = len(list_of_numbers)
-stored_pairs = []
-i = 0
-j = 0
-while i <= len(list_of_numbers):
-    while j <= len(list_of_numbers):
-        if list_of_numbers[i] + list_of_numbers[j] == target_number:
-            stored_pairs.append((i,j))
-            list_of_numbers.pop(i)
-            list_of_numbers.pop(j)
-            print(list_of_numbers[i])
-            print(list_of_numbers[j])
-        j+=1
-    i+=1                
-print(stored_pairs)
 
-""" 
 stored_pairs=[]
 for i in list_of_numbers:
     for j in list_of_numbers:
         if i + j == target_number:
+            if (i in list_of_numbers_copy) and (j in list_of_numbers_copy) and i!=j:
+                list_of_numbers_copy.remove(i)
+                list_of_numbers_copy.remove(j)
+            else:
+                continue                    
             stored_pairs.append((i,j))
-            list_of_numbers_copy.remove(i)
-            list_of_numbers_copy.remove(j)
-print(stored_pairs) """
+
+lst = [i for i in list_of_numbers_copy if i == int(target_number/2) and target_number%2==0]  # If the target number is even, I'll have pairs of equal numbers       
+
+pairs = [(lst[i], lst[i+1]) for i in range(0, len(lst) - 1, 2)] # This are all the pairs of equal numbers
+remaining = lst[-1] if len(lst) % 2 != 0 else [] # This is the remaining one
+stored_pairs += pairs # I add those to the stored pairs
+list_of_numbers_copy = [i for i in list_of_numbers_copy if i!=int(target_number/2)] # I take those out
+list_of_numbers_copy += [remaining] # I add the remaining one, if there's any
+print(stored_pairs)  
+print(list_of_numbers_copy)
+
