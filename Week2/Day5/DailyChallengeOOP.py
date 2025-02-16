bf6ed33430b86dae7d52a3a1b237901514caedba
Julieta Wenger <wenerjulieta@gmail.com>
2025-02-16 17:29:58 +0200

    daily challenge thursday

--- Week2/Day5/DailyChallengeOOP.py
+++ Week2/Day5/DailyChallengeOOP.py
@@ -1,3 +1,36 @@
+""" Part 1 : Quizz :
+
+Answer the following questions
+
+    1. What is a class?
+    2. What is an instance?
+    3. What is encapsulation?
+    4. What is abstraction?
+    5. What is inheritance?
+    6. What is multiple inheritance?
+    7. What is polymorphism?
+    8. What is method resolution order or MRO?
+"""
+"""
+Classes in Python are blueprints for creating objects. They define the attributes (data) and methods (functions) 
+that objects of the class will have. Objects are instances of classes. 
+An instance is a realization of that blueprint.
+When you create an instance of a class, you are essentially creating a unique copy of the class with its own set of attributes and methods. 
+This allows for the creation of multiple independent objects with similar characteristics.
+Encapsulation is the practice of obscuring data or implementation details within a class so that its internal operations remain hidden.
+Abstraction is a process of hiding unnecessary details and showing only the essential information to the user.
+Inheritance is the process by which one class takes on the attributes and methods of another.
+Moreover, a class can inherit not only from one class, but from different classes. This is multiple inheritance.
+In programming, polymorphism means different or related classes that use the same names for their functions.
+Polymorphism allows the ability to use a standard interface for multiple forms or data types.
+Method Resolution Order (MRO) is the order in which Python looks for a method in a hierarchy of classes. 
+Especially it plays vital role in the context of multiple inheritance as single method may be found in multiple super classes.
+"""
+
+
+
+
+
 """ 
 Part 2: Create a deck of cards class.
 
@@ -19,7 +52,7 @@ class Card:
 
 class DeckOfCards:    
     # This doesn't look particularly optimized
-    
+
     suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
     values = ["A",2,3,4,5,6,7,8,9,10,"J","Q", "K"]  
     def __init__(self):
